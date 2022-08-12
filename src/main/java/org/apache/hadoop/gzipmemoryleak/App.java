3d2fa629b2b5ec98f76a7bf990a00b2ec0c7f654
Kevin Sewell <kevins_25@apple.com>
2022-08-13 01:41:44 +0200

    feat: handling CompressionInputStream

--- src/main/java/org/apache/hadoop/gzipmemoryleak/App.java
+++ src/main/java/org/apache/hadoop/gzipmemoryleak/App.java
@@ -1,9 +1,11 @@
 package org.apache.hadoop.gzipmemoryleak;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.compress.CompressionInputStream;
 import org.apache.hadoop.io.compress.CompressionOutputStream;
 import org.apache.hadoop.io.compress.GzipCodec;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Random;
@@ -21,9 +23,23 @@ public class App {
 
         final Random random = new Random();
         while (true) {
-            try (CompressionOutputStream outputStream = gzipCodec.createOutputStream(new ByteArrayOutputStream())) {
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            try (CompressionOutputStream outputStream = gzipCodec.createOutputStream(baos)) {
                 for (int i = 0; i < fileSize; i++) {
                     outputStream.write(random.nextInt(256));
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            byte[] bytes = baos.toByteArray();
+            ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+
+            try (CompressionInputStream outputStream = gzipCodec.createInputStream(bais)) {
+                for (int i = 0; i < fileSize; i++) {
+                    outputStream.read();
                 }
             } catch (IOException e) {
                 throw new RuntimeException(e);
